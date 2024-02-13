d4a01011210ef80fcbc4ff7859959be7a02389f9
abay.ongar <abay.ongar.botabek@gmail.com>
2024-02-13 11:39:50 +0600

    after adding ship to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,20 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion: 
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800, 400))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (7, 54, 66)
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -17,7 +22,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
             self.clock.tick(60)
