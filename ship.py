fd00b0cc2a97c230d03af792e067f78d9d673caf
abay.ongar <abay.ongar.botabek@gmail.com>
2024-02-14 09:56:42 +0600

    solga jane onga jyljytu

--- ship.py
+++ ship.py
@@ -10,5 +10,11 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.moving_right = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
