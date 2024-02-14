3ec4b221b292179a93bed2b99cbff9647b24c154
abay.ongar <abay.ongar.botabek@gmail.com>
2024-02-14 10:53:33 +0600

    after fixing some error on midbootom

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         #Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2
--- ship.py
+++ ship.py
@@ -3,6 +3,7 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
         self.image = pygame.image.load('images/ship.bmp')
